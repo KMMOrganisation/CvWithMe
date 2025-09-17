3fdf9a8614e613afbff79a21ce2f95077db1d063
Katie <137323762+KatieM00@users.noreply.github.com>
2025-09-17 20:35:00 +0100

    Integrating course content

--- src/data/courseContent.ts
+++ src/data/courseContent.ts
@@ -2379,6 +2379,6 @@ export const HAS_COURSE_CONTENT = true;
  */
 export const COURSE_METADATA = {
   contentLength: 153059,
-  generatedAt: '2025-09-17T19:28:11.843Z',
+  generatedAt: '2025-09-17T19:34:32.080Z',
   source: 'Course.md'
 };
